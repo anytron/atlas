8c9fe163da91cf5ae117fd833879986336aacb40
anytron <anytron@foxmail.com>
2020-02-06 21:33:40 +0800

    db c

---
+++ db/user.sql
@@ -0,0 +1,14 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 8.0.18 
+*********************************************************************
+*/
+/*!40101 SET NAMES utf8 */;
+
+create table `user` (
+	`id` bigint (20),
+	`name` varchar (600),
+	`age` int (3),
+	`sex` int (1),
+	`addr` varchar (900)
+); 
--- readme.md
+++ readme.md
@@ -12,5 +12,9 @@
 * 安全[最后做]
 * 网关
 * config[最后做]
-* 监控[最后做]
+* 监控
+
+项目中采用的maven jar均为较高版本的配置
+注意数据库采用的连接为8+版本 连接驱动会跟之前不太一样 
+atlas-provider-user下的application.yml driver-class-name: com.mysql.cj.jdbc.Driver
 
